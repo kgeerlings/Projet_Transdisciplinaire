dd4185f89c452a1501f6ca72ccf95eb8dc8ace24
GarrabieAubies <150822741+GarrabieAubies@users.noreply.github.com>
2024-05-22 00:39:10 +0200

    Update Developpement.js

--- projetTransDi/src/pages/Developpement/Developpement.js
+++ projetTransDi/src/pages/Developpement/Developpement.js
@@ -1,47 +1,80 @@
-import React from 'react';
-import './Developpement.css';
+import React, { useEffect, useState } from "react";
+import "./Developpement.css";
 
-import Navbar from '../../components/NavBar/Navbar';
-import Subtitle from '../../components/Subtitle/Subtitle';
-import Footer from '../../components/Footer/Footer';
-
+import Navbar from "../../components/NavBar/Navbar";
+import Subtitle from "../../components/Subtitle/Subtitle";
+import Footer from "../../components/Footer/Footer";
 
 function Developpement({ setPage }) {
-    return (
-        <div className="Developpement">
-            <Navbar setPage={setPage} />
-            <div id="dev">
-                <div id="para">
-                    <Subtitle nom="Attendus de la cliente" />
-                    <p>Lors du premier rendez-vous avec la cliente, on lui a demandé particulièrement de nous présenter le projet et le jeu en lui-même. Ensuite, on lui a poser la question des choses qu'elle aimerait implémenter ou améliorer. Ce que l'on a conclut suite à cette première réunion sont les points suivantes:</p>
-                    <ul>
-                        <li> Une phase d'étude: particulièrement, tester le jeu sur des enfants.</li>
-                        <li>Une phase de développement</li>
-                    </ul>
-                    <p> Pour la phase de développement, elle nous avait donné plusieurs idées de ce qu'elle aimerait mettre en place, donc nous nous sommes fixés des objectifs à partir de cela.</p>
-                </div>
-                <div id="objectifs">
-                    <Subtitle nom="Nos objectifs" />
-                    <h3> La phase d'étude</h3>
-                    <p> Pour la phase d'étude, nous pensions que ce serait intéressant de tester le jeu sur des enfants "normaux" mais aussi sur des enfants atteint de dycaculie. Notre objectif pour la phase d'étude était donc:</p>
-                    <ul>
-                        <li>Tester le jeu sur des enfants de profils divers</li>
-                        <li>Mieux comprendre ce qu'est la dyscalculie</li>
-                        <li>Avoir une vue globale du fonctionnement du jeu</li>
-                    </ul>
-                    <br></br>
-                    <h3> La phase de développement</h3>
-                    <p> Pour la phase de développement, nous savions d'avance que ce ne serait pas réalisable d'implémenter tout ce que souhaitait la cliente donc nous nous sommes focaliser sur:</p>
-                    <ul>
-                        <li>L'amélioration de l'interface des professeurs</li>
-                        <li>Questionnaire de ressenti de l'enfant</li>
-                        <li>Conception de nouveaux jeux</li>
-                    </ul>
-                </div>
-            </div>
-            <Footer />
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    const targetValue = 8720; // Replace with your target value
+    const duration = 2000; // Duration of the animation in milliseconds
+
+    const startTime = Date.now();
+    const updateCounter = () => {
+      const elapsedTime = Date.now() - startTime;
+      const progress = Math.min(elapsedTime / duration, 1);
+      setCounter(Math.floor(progress * targetValue));
+
+      if (progress < 1) {
+        requestAnimationFrame(updateCounter);
+      }
+    };
+
+    requestAnimationFrame(updateCounter);
+  }, []);
+  return (
+    <div className="Developpement">
+      <Navbar setPage={setPage} />
+      <div id="dev">
+        <div class="Phase">
+          <div class="titre">Phase de développement 1</div>
+          <h3>Compréhension de la structure</h3> Il est évident que l'un des
+          enjeu les plus important lorsqu'il s'agit de reprendre un projet
+          informatique est de comprendre la structure de celui-ci. Pour cela
+          nous avons commencé par analyser le code source du projet. Nous avons
+          donc pu comprendre les différentes classes et fonctions qui composent
+          le projet. Nous avons également pu comprendre les différentes
+          fonctionnalités du projet. Ainsi que les formats de fichiers utilisés
+          pour les niveaux et les éléments du jeu. Nous verrons par la suite
+          comment nous avons utilisé ces informations pour la suite du
+          développement.
+          <div className="chiffreImpressionant">{counter} Mo</div>
+          de code source analysé
         </div>
-    )
-};
+        <div class="Phase">
+          <div class="titre">Phase de développement 2</div>
+          <h3>Création de l'éditeur de jeu</h3> Pour la création d'un étideur de
+          niveau sur unity nous avons décider de partir sur un éditeur de niveau
+          simple et intuitif. Mais avec le plus de modulatrié possible pour que
+          l'éditeur soit le plus complet et s'adapte à tous les besoins. Pour
+          cela nous avons décidé de partir sur un éditeur de niveau en 2D. En
+          utilisant des images, étant le support le plus simple pour les
+          utilisateur (enfants et enseignant).
+          <div class="titre">Video Demonstration</div>
+          <iframe
+            style={{
+              padding: "1vw",
+              width: "40vw",
+              height: "25vw",
+            }}
+            src="https://www.youtube.com/embed/1YkGf_xTs0o"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+          <p>
+            {" "}
+            Pour cela nous nous somme inspiré d'un site web de jeu avec des
+            mécaniques de drag and drop d'éléments.
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
 
 export default Developpement;
